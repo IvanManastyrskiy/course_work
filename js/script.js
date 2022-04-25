a35aace18999c1bf8f9dd501da224f38ea6957f7
Maksim2203 <98012684+Maksim2203@users.noreply.github.com>
2022-04-25 19:18:33 +0300

    р

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length - 1; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
